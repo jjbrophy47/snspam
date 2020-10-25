f616299b9fd699c09289484b6051473c828e38dd
jbrophy <jonathanbrophy47@gmail.com>
2020-10-25 14:16:18 -0700

    fixing script

--- jobs/soundcloud/primer.sh
+++ jobs/soundcloud/primer.sh
@@ -5,7 +5,7 @@ partition=$3
 sbatch --mem=${mem}G \
        --time=$time \
        --partition=$partition \
-       --job-name=soundcloud \
-       --output=jobs/logs/soundcloud/ \
-       --error=jobs/errors/soundcloud/ \
+       --job-name=preprocess \
+       --output=jobs/logs/soundcloud/preprocess \
+       --error=jobs/errors/soundcloud/preprocess \
        jobs/soundcloud/runner.sh
