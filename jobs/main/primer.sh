d9bb0d0ae92d06c4d73cbd4bb374bd231ae90caf
jbrophy <jonathanbrophy47@gmail.com>
2020-11-04 08:23:25 -0800

    updated batch

--- jobs/main/primer.sh
+++ jobs/main/primer.sh
@@ -7,12 +7,13 @@ featureset_list=('limited' 'full')
 setting_list=('inductive' 'inductive+transductive')
 stacks_list=(0 1 2)
 joint_list=('None' 'mrf' 'psl')
-
-featureset_list=('limited')
-setting_list=('inductive')
-stacks_list=(0)
-joint_list=('psl')
 fold_list=(0 1 2 3 4 5 6 7 8 9)
+
+# featureset_list=('limited' 'full')
+# setting_list=('inductive' 'inductive+transductive')
+# stacks_list=(0 1 2)
+joint_list=('None' 'mrf')
+# fold_list=(0 1 2 3 4 5 6 7 8 9)
 
 for fold in ${fold_list[@]}; do
     for featureset in ${featureset_list[@]}; do
